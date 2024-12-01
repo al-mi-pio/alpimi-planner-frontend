4887880d840392c8e15f79187325d1bb7a3357fd
ThePanToster <alcrafter@o2.pl>
2024-12-01 02:52:51 +0100

    Add dimensions and colors constants

---
+++ src/shared/constants/colors.ts
@@ -0,0 +1,23 @@
+export const darkColors = {
+    secondaryText: '#0E1416',
+    primaryText: '#F2F2F2',
+    sectionBackground: '#30292F',
+    primaryBackground: '#282127',
+    elementBackground: '#3D3A41',
+    primaryAccent: '#A0A1AE',
+    error: '#ED505F',
+    success: '#49A95C',
+    highlight: '#1D8EE5',
+} as const;
+
+export const lightColors = {
+    secondaryText: '#0E1416',
+    primaryText: '#F2F2F2',
+    sectionBackground: '#30292F',
+    primaryBackground: '#282127',
+    elementBackground: '#3D3A41',
+    primaryAccent: '#A0A1AE',
+    error: '#ED505F',
+    success: '#49A95C',
+    highlight: '#1D8EE5',
+} as const;
---
+++ src/shared/constants/dimensions.ts
@@ -0,0 +1,29 @@
+export const fontSizes = {
+    large3Xl: '80pt',
+    largeXXL: '40pt',
+    largeXL: '32pt',
+    large: '26pt',
+    regular: '20pt',
+    small: '16pt',
+} as const;
+
+export const lineHeights = {
+    large3Xl: '97pt',
+    largeXXL: '48pt',
+    largeXL: '39pt',
+    large: '31pt',
+    regular: '24pt',
+    small: '19pt',
+} as const;
+
+export const sizes = {
+    large3Xl: '10em',
+    largeXXL: '8em',
+    largeXL: '6em',
+    large: '4em',
+    regular: '2em',
+    small: '1em',
+    smallXL: '0.5em',
+    smallXXL: '0.2em',
+    small3XL: '0.1em',
+} as const;
