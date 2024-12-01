ded9ad390f54f43f37a9a7c8f3ca730e958d2159
ThePanToster <alcrafter@o2.pl>
2024-12-01 20:35:45 +0100

    Update light theme colors

--- src/shared/constants/colors.ts
+++ src/shared/constants/colors.ts
@@ -12,11 +12,11 @@ export const darkColors = {
 
 export const lightColors = {
     secondaryText: '#0E1416',
-    primaryText: '#F2F2F2',
-    sectionBackground: '#30292F',
-    primaryBackground: '#282127',
-    elementBackground: '#3D3A41',
-    primaryAccent: '#A0A1AE',
+    primaryText: '#0E1416',
+    sectionBackground: '#E3E3E3',
+    primaryBackground: '#C9C8C8',
+    elementBackground: '#FEFEFE',
+    primaryAccent: '#FFFFFF',
     error: '#ED505F',
     success: '#49A95C',
     highlight: '#1D8EE5',
