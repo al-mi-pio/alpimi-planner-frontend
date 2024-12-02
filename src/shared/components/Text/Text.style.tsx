9b83afefd6565d739cbe9d63fd162c1821549828
ThePanToster <alcrafter@o2.pl>
2024-12-02 15:20:17 +0100

    Add stories and new theme styles for Text.tsx

--- src/shared/components/Text/Text.style.tsx
+++ src/shared/components/Text/Text.style.tsx
@@ -1,13 +1,8 @@
 import styled from 'styled-components';
 
-import { darkColors, lightColors } from '@/shared/constants/colors.ts';
-import {
-    fontSizes,
-    lineHeights,
-    sizes,
-} from '@/shared/constants/dimensions.ts';
+import { fontSizes, lineHeights, sizes } from '@/shared/constants/dimensions';
 
-const StyledInput = styled.input`
+export const StyledInput = styled.input<{ $error?: boolean }>`
     & {
         font-size: ${fontSizes.small};
         line-height: ${lineHeights.small};
@@ -15,54 +10,32 @@ const StyledInput = styled.input`
         border: none;
         border-radius: ${sizes.smallXL};
         width: 300px;
+        color: ${(props) => props.theme.colors.secondaryText};
+        background-color: ${(props) => props.theme.colors.primaryAccent};
+        outline: 1px solid ${(props) => props.theme.colors.primaryText};
+        ${(props) =>
+            props.$error
+                ? `outline: 2px solid ${props.theme.colors.error};
+                   color: ${props.theme.colors.error};`
+                : ''};
     }
     &:focus {
-        outline: 2px solid ${darkColors.highlight};
+        outline: 2px solid ${(props) => props.theme.colors.highlight};
     }
 `;
 
-export const DarkInput = styled(StyledInput)<{ $error?: boolean }>`
-    color: ${darkColors.secondaryText};
-    background-color: ${darkColors.primaryAccent};
-    outline: 1px solid ${darkColors.primaryText};
-    ${(props) =>
-        props.$error ? `outline: 2px solid ${darkColors.error};` : ''};
-`;
-
-export const LightInput = styled(StyledInput)<{ $error?: boolean }>`
-    color: ${lightColors.secondaryText};
-    background-color: ${lightColors.primaryAccent};
-    outline: 1px solid ${lightColors.primaryText};
-    ${(props) =>
-        props.$error ? `outline: 2px solid ${lightColors.error};` : ''};
-`;
-
-const StyledLabel = styled.p`
+export const StyledLabel = styled.p`
     font-size: ${fontSizes.small};
     line-height: ${lineHeights.small};
     margin-bottom: ${sizes.smallXXL};
     margin-left: ${sizes.smallXL};
+    color: ${(props) => props.theme.colors.primaryText};
 `;
 
-export const DarkLabel = styled(StyledLabel)`
-    color: ${darkColors.primaryText};
-`;
-
-export const LightLabel = styled(StyledLabel)`
-    color: ${lightColors.primaryText};
-`;
-
-const StyledDescription = styled.p`
+export const StyledDescription = styled.p`
     font-size: ${fontSizes.small};
     line-height: ${lineHeights.small};
     margin-top: ${sizes.smallXXL};
     margin-left: ${sizes.smallXL};
-`;
-
-export const DarkDescription = styled(StyledDescription)`
-    color: ${darkColors.error};
-`;
-
-export const LightDescription = styled(StyledDescription)`
-    color: ${lightColors.error};
+    color: ${(props) => props.theme.colors.error};
 `;
