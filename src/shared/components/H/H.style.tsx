f8f17f0304449b281f0a739e4e798eed25dc25e9
ThePanToster <alcrafter@o2.pl>
2025-02-28 21:14:43 +0100

    Extract focus styles

--- src/shared/components/H/H.style.tsx
+++ src/shared/components/H/H.style.tsx
@@ -1,11 +1,10 @@
 import styled from 'styled-components';
 
-import { HeadingLevel } from '@/shared/components/H/H';
+import { HeadingLevel } from '@/shared/components/H/types';
 import {
     fontSizes,
     lineHeights,
     fontWeights,
-    sizes,
 } from '@/shared/constants/dimensions';
 
 export const StyledH = styled.p<{
@@ -14,8 +13,7 @@ export const StyledH = styled.p<{
     $secondary?: boolean;
 }>`
     font-size: ${(props) => Object.values(fontSizes)[props.$level - 1]};
-    line-height: ${lineHeights.small};
-    margin: ${sizes.smallXL} 0;
+    line-height: ${(props) => Object.values(lineHeights)[props.$level - 1]};
     font-weight: ${(props) =>
         props.$bold ? fontWeights.bold : fontWeights.regular};
     color: ${(props) =>
--- src/shared/components/P/P.style.tsx
+++ src/shared/components/P/P.style.tsx
@@ -4,13 +4,11 @@ import {
     fontSizes,
     lineHeights,
     fontWeights,
-    sizes,
 } from '@/shared/constants/dimensions';
 
 export const StyledP = styled.p<{ $bold?: boolean; $secondary?: boolean }>`
     font-size: ${fontSizes.small};
     line-height: ${lineHeights.small};
-    margin: ${sizes.smallXL} 0;
     font-weight: ${(props) =>
         props.$bold ? fontWeights.bold : fontWeights.regular};
     color: ${(props) =>
