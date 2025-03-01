1fdeb64ff00d439c802ff1ec24172a12ce2e1d95
ThePanToster <alcrafter@o2.pl>
2025-03-01 23:22:34 +0100

    Add some icons

---
+++ src/shared/icons/SortDown.tsx
@@ -0,0 +1,19 @@
+import { Icon } from '@/shared/icons/Icon.style';
+
+const SortDown = () => (
+    <Icon
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        width="50"
+        height="50"
+        viewBox="0 0 50 50"
+    >
+        <path
+            fill="currentColor"
+            d="M4 13H46C49 13 50 15 48 17L27 36C26 37 24 37 23 36L2 17C0 15 1 13 4 13Z"
+        />
+    </Icon>
+);
+
+export default SortDown;
