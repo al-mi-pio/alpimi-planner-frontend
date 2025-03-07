cddf99811c1d4aead0b8072a1ac4750e9c10064f
ThePanToster <alcrafter@o2.pl>
2025-03-07 01:39:47 +0100

    Create Checkbox component

---
+++ src/shared/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,38 @@
+import { useId } from 'react';
+
+import {
+    CheckboxWrapper,
+    StyledCheckbox,
+} from '@/shared/components/Checkbox/Checkbox.style';
+import P from '@/shared/components/P';
+import { ErrorDescription } from '@/shared/styles/Strings';
+import { InputProps } from '@/shared/types/inputs';
+
+/**
+ * A UI component which accepts user boolean input
+ */
+const Checkbox = ({ label, error, ...defaultProps }: InputProps) => {
+    const errorDescriptionId = useId();
+    return (
+        <label>
+            <CheckboxWrapper>
+                <StyledCheckbox
+                    $error={!!error}
+                    aria-invalid={!!error}
+                    aria-describedby={error ? errorDescriptionId : undefined}
+                    type="checkbox"
+                    {...defaultProps}
+                />
+                {label && <P>{label}</P>}
+            </CheckboxWrapper>
+
+            {error && (
+                <ErrorDescription role="alert" id={errorDescriptionId}>
+                    {error}
+                </ErrorDescription>
+            )}
+        </label>
+    );
+};
+
+export default Checkbox;
