ad4bdbaa523eb12bf7e8988e6db413efec7070e0
ThePanToster <alcrafter@o2.pl>
2025-03-10 10:47:11 +0100

    Add Link unit tests

---
+++ src/shared/components/Link/Link.unit.tsx
@@ -0,0 +1,14 @@
+import { describe, expect, test } from 'vitest';
+
+import Link from '@/shared/components/Link';
+import { render, screen } from '@/shared/test-utils/render';
+
+describe('Link.tsx', () => {
+    test('Render correct text', () => {
+        render(<Link href="https://www.example.com/">{'Test text'}</Link>);
+
+        const link = screen.getByRole('link', { name: 'Test text' });
+
+        expect(link).toHaveProperty('href', 'https://www.example.com/');
+    });
+});
