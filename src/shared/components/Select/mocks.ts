a88b4cb1e6731999c132cb4451345698d277075e
ThePanToster <alcrafter@o2.pl>
2025-06-14 03:59:54 +0200

    Add mocks and types

---
+++ src/shared/components/Select/mocks.ts
@@ -0,0 +1,12 @@
+export const selectOptionsMock = [
+    { value: 'blue', label: 'Blue' },
+    { value: 'red', label: 'Red' },
+    { value: 'green', label: 'Green' },
+    { value: 'purple', label: 'Purple' },
+    { value: 'yellow', label: 'Yellow' },
+    { value: 'gold', label: 'Gold' },
+    { value: 'silver', label: 'Silver' },
+    { value: 'orange', label: 'Orange' },
+    { value: 'cyan', label: 'Cyan' },
+    { value: 'magenta', label: 'Magenta' },
+];
