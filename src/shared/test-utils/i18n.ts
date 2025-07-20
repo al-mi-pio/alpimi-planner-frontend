295da0bace4c0cada67717a618396eb07befd6f6
ThePanToster <alcrafter@o2.pl>
2025-07-20 03:14:39 +0200

    Fix test i18n config

--- src/shared/test-utils/i18n.ts
+++ src/shared/test-utils/i18n.ts
@@ -1,7 +1,7 @@
 import { initReactI18next } from 'react-i18next';
 
 // eslint-disable-next-line import-x/extensions
-import english from '../../../public/locales/en/general.json';
+import english from '../../../public/locales/en-US/general.json';
 // eslint-disable-next-line import-x/extensions
 import polish from '../../../public/locales/pl/general.json';
 import i18n from 'i18next';
@@ -11,13 +11,13 @@ import Backend from 'i18next-http-backend/cjs';
 i18n.use(initReactI18next)
     .use(Backend)
     .init({
-        lng: 'en',
-        fallbackLng: 'en',
+        lng: 'en-US',
+        fallbackLng: 'en-US',
         ns: ['general'],
         defaultNS: 'general',
         debug: true,
         resources: { en: { general: english }, pl: { general: polish } },
     })
-    .then(() => {});
+    .then();
 
 export default i18n;
