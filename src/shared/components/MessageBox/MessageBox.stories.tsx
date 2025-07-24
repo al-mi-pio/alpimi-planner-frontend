c7cd77ea53a7730b205d57e9f5d933e5c157afba
ThePanToster <alcrafter@o2.pl>
2025-07-24 21:13:19 +0200

    Add MessageBox stories

---
+++ src/shared/components/MessageBox/MessageBox.stories.tsx
@@ -0,0 +1,57 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { fn } from '@storybook/test';
+
+import MessageBox from '@/shared/components/MessageBox';
+import { MessageType } from '@/shared/types';
+
+export default {
+    title: 'Shared/Components/MessageBox',
+    argTypes: {
+        children: {
+            description: 'Text that will be rendered',
+        },
+    },
+    component: MessageBox,
+} satisfies Meta<typeof MessageBox>;
+
+export const Success: StoryObj<typeof MessageBox> = {
+    args: {
+        type: MessageType.success,
+        children: 'Example message',
+        onClose: fn(),
+    },
+};
+
+export const Warning: StoryObj<typeof MessageBox> = {
+    args: {
+        type: MessageType.warning,
+        children: 'Example message',
+        onClose: fn(),
+    },
+};
+
+export const Info: StoryObj<typeof MessageBox> = {
+    args: {
+        type: MessageType.info,
+        children: 'Example message',
+        onClose: fn(),
+    },
+};
+
+export const Error: StoryObj<typeof MessageBox> = {
+    args: {
+        type: MessageType.error,
+        children: 'Example message',
+        onClose: fn(),
+    },
+};
+
+export const NoCloseButton: StoryObj<typeof MessageBox> = {
+    args: {
+        type: MessageType.error,
+        children: 'Example message',
+        noClosing: true,
+        onClose: fn(),
+    },
+};
