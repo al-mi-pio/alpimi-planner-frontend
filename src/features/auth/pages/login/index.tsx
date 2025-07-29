47d826b7d1475bbef6f8bf4f01b361030caba608
ThePanToster <alcrafter@o2.pl>
2025-07-29 02:58:04 +0200

    Add missing logic

--- src/features/auth/pages/login/index.tsx
+++ src/features/auth/pages/login/index.tsx
@@ -1,6 +1,8 @@
 import { useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 import { AxiosError } from 'axios';
 
@@ -11,12 +13,18 @@ import { AuthForm } from '@/features/auth/styles/Auth.style';
 import Button from '@/shared/components/Button';
 import Image from '@/shared/components/Image';
 import Link from '@/shared/components/Link';
-import P from '@/shared/components/P';
+import LoadingBox from '@/shared/components/LoadingBox';
+import MessageBox from '@/shared/components/MessageBox';
 import Text from '@/shared/components/Text';
+import { MessageType } from '@/shared/types';
 
 const LoginPage = () => {
     const { t } = useTranslation('auth');
-    const [message, setMessage] = useState('');
+    const [params] = useSearchParams();
+    const navigate = useNavigate();
+    const [message, setMessage] = useState(
+        params.has('redirect') ? t('Session expired, please sign in again') : ''
+    );
     const [credentials, setCredentials] = useState<AuthDTO>({
         login: '',
         password: '',
@@ -25,11 +33,11 @@ const LoginPage = () => {
     const onSuccess = ({ content }: AuthResponse) => {
         localStorage.setItem('accessToken', content);
         setMessage(t('Login successful'));
+        navigate(params.get('redirect') ?? '/dashboard');
     };
     const onError = (response: ErrorResponse | AxiosError) => {
         if ('errors' in response) setMessage(response.errors[0].message);
-        // TODO: Replace with Toast
-        else console.log(response.message);
+        else toast.error(t(response.message, { ns: 'general' }));
     };
 
     const { isPending, mutate: login } = useMutation({
@@ -38,53 +46,65 @@ const LoginPage = () => {
         onSuccess,
     });
 
-    // TODO: Replace with LoadingBox
-    if (isPending) return <P>{'Loading...'}</P>;
-
     return (
-        <AuthForm>
-            <Image
-                id="alpimi-logo"
-                src="/images/alpimilogo_trans.webp"
-                alt="Alpimi Planner logo"
-                width="236px"
-            />
+        <LoadingBox loading={isPending}>
+            <AuthForm>
+                <Image
+                    id="alpimi-logo"
+                    src="/images/alpimilogo_trans.webp"
+                    alt="Alpimi Planner logo"
+                    width="200px"
+                />
 
-            {
-                // TODO: Replace with MessageBox
-                message && <P>{message}</P>
-            }
+                {message && (
+                    <MessageBox
+                        type={
+                            message === t('Login successful')
+                                ? MessageType.success
+                                : MessageType.error
+                        }
+                        noClosing
+                    >
+                        {message}
+                    </MessageBox>
+                )}
 
-            <Text
-                label={t('Username')}
-                value={credentials.login}
-                onChange={({ target }) =>
-                    setCredentials((prev) => ({ ...prev, login: target.value }))
-                }
-            />
+                <Text
+                    label={t('Login')}
+                    name="login"
+                    value={credentials.login}
+                    onChange={({ target }) =>
+                        setCredentials((prev) => ({
+                            ...prev,
+                            login: target.value,
+                        }))
+                    }
+                />
 
-            <Text
-                label={t('Password')}
-                type="password"
-                value={credentials.password}
-                onChange={({ target }) =>
-                    setCredentials((prev) => ({
-                        ...prev,
-                        password: target.value,
-                    }))
-                }
-            />
+                <Text
+                    label={t('Password')}
+                    name="password"
+                    type="password"
+                    value={credentials.password}
+                    onChange={({ target }) =>
+                        setCredentials((prev) => ({
+                            ...prev,
+                            password: target.value,
+                        }))
+                    }
+                />
 
-            <Button
-                onClick={(e) => {
-                    e.preventDefault();
-                    login(credentials);
-                }}
-                label={t('Login')}
-            />
+                <Button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        login(credentials);
+                    }}
+                    label={t('Sign in')}
+                />
 
-            <Link href="/resetpassword">{t('I forgot my password')}</Link>
-        </AuthForm>
+                <Link href="/resetpassword">{t('I forgot my password')}</Link>
+            </AuthForm>
+        </LoadingBox>
     );
 };
 
