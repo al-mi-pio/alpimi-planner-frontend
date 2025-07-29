a76311c7b1a25dc56c30af9fe2e89616649117d1
ThePanToster <alcrafter@o2.pl>
2025-07-29 02:57:25 +0200

    Add Toast to Auth pages

--- src/features/auth/template/index.tsx
+++ src/features/auth/template/index.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from 'react';
 
 import { AuthBodyStyles, CenterBox } from '@/features/auth/styles/Auth.style';
+import Toast from '@/shared/components/Toast';
 
 const Auth = ({ children }: { children: ReactNode }) => (
     <>
         <AuthBodyStyles />
         {!!children && <CenterBox>{children}</CenterBox>}
+        <Toast />
     </>
 );
 
