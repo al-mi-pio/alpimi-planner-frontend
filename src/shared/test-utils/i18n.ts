af38a3a487131aaa4cde838dead67a0ba0ffe91c
ThePanToster <alcrafter@o2.pl>
2025-08-21 23:55:58 +0200

    Add schedules locales to unit tests

--- src/shared/test-utils/i18n.ts
+++ src/shared/test-utils/i18n.ts
@@ -5,9 +5,13 @@ import auth_en_us from '../../../public/locales/en-US/auth.json';
 // eslint-disable-next-line import-x/extensions
 import general_en_us from '../../../public/locales/en-US/general.json';
 // eslint-disable-next-line import-x/extensions
+import schedules_en_us from '../../../public/locales/en-US/schedules.json';
+// eslint-disable-next-line import-x/extensions
 import auth_pl from '../../../public/locales/pl/auth.json';
 // eslint-disable-next-line import-x/extensions
 import general_pl from '../../../public/locales/pl/general.json';
+// eslint-disable-next-line import-x/extensions
+import schedules_pl from '../../../public/locales/pl/schedules.json';
 import i18n from 'i18next';
 import Backend from 'i18next-http-backend/cjs';
 
@@ -17,12 +21,15 @@ i18n.use(initReactI18next)
     .init({
         lng: 'en-US',
         fallbackLng: 'en-US',
-        ns: ['general', 'auth'],
+        ns: ['general', 'auth', 'schedules'],
         defaultNS: 'general',
-        debug: true,
         resources: {
-            'en-US': { general: general_en_us, auth: auth_en_us },
-            pl: { general: general_pl, auth: auth_pl },
+            'en-US': {
+                general: general_en_us,
+                auth: auth_en_us,
+                schedules: schedules_en_us,
+            },
+            pl: { general: general_pl, auth: auth_pl, schedules: schedules_pl },
         },
     })
     .then();
