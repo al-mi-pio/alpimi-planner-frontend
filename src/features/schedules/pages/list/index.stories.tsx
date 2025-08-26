486cd19651da85114777d2002967c7646b292e12
ThePanToster <alcrafter@o2.pl>
2025-08-27 01:18:16 +0200

    Add schedules page stories

---
+++ src/features/schedules/pages/list/index.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import Dashboard from '@/features/dashboard/template';
+import { schedulesDashboardHeader } from '@/features/schedules/constants';
+import SchedulesPage from '@/features/schedules/pages/list/index';
+import { PageStoryStyles } from '@/shared/styles/Stories';
+
+export default {
+    title: 'Pages/Schedules',
+    component: SchedulesPage,
+    render: () => (
+        <Dashboard headerProps={schedulesDashboardHeader}>
+            <PageStoryStyles />
+            <SchedulesPage />
+        </Dashboard>
+    ),
+} satisfies Meta<typeof SchedulesPage>;
+
+export const Default: StoryObj<typeof SchedulesPage> = {};
