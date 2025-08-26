867ddca53274f3f825f94b6447c533d4b6687221
ThePanToster <alcrafter@o2.pl>
2025-08-27 01:14:16 +0200

    Add styles for page stories

--- src/features/auth/pages/login/index.stories.tsx
+++ src/features/auth/pages/login/index.stories.tsx
@@ -2,12 +2,14 @@ import { Meta, StoryObj } from '@storybook/react';
 
 import LoginPage from '@/features/auth/pages/login';
 import Auth from '@/features/auth/template';
+import { PageStoryStyles } from '@/shared/styles/Stories';
 
 export default {
     title: 'Pages/Login',
     component: LoginPage,
     render: () => (
         <Auth>
+            <PageStoryStyles />
             <LoginPage />
         </Auth>
     ),
