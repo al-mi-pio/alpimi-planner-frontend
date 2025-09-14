9a3ebdaa6695be2811617bce940c144863d0f6d6
ThePanToster <alcrafter@o2.pl>
2025-09-14 22:31:56 +0200

    Add useMutateData hook

---
+++ src/shared/hooks/useMutateData.tsx
@@ -0,0 +1,52 @@
+import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+
+import type { DefaultError } from '@tanstack/query-core';
+import { AxiosError } from 'axios';
+
+import { ErrorResponse } from '@/api/types';
+import { login } from '@/shared/constants/routes';
+
+export const useMutateData = <
+    TData = unknown,
+    TError = DefaultError,
+    TVariables = void,
+    TContext = unknown,
+>(
+    mutationOptions: UseMutationOptions<TData, TError, TVariables, TContext>
+) => {
+    const navigate = useNavigate();
+    const { t } = useTranslation('general');
+    const handleError = (
+        error: TError,
+        variables: TVariables,
+        context?: TContext
+    ) => {
+        const result = error as ErrorResponse | AxiosError;
+        if (result)
+            if ('errors' in result) {
+                if (result.status === 401)
+                    navigate({
+                        pathname: login,
+                        search:
+                            '?redirect=' +
+                            encodeURIComponent(
+                                location.search
+                                    ? location.pathname + '?' + location.search
+                                    : location.pathname
+                            ),
+                    });
+            } else toast.error(t(result.message));
+
+        if (mutationOptions.onError)
+            mutationOptions.onError(error, variables, context);
+    };
+
+    return useMutation({
+        retry: false,
+        ...mutationOptions,
+        onError: handleError,
+    });
+};
