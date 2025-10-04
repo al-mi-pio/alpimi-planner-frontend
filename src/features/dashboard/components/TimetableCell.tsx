81e714d4fe8aa142aa8105036816d64b02700eaf
ThePanToster <alcrafter@o2.pl>
2025-10-05 01:29:23 +0200

    Add TimetableCell component

---
+++ src/features/dashboard/components/TimetableCell.tsx
@@ -0,0 +1,55 @@
+import { ComponentPropsWithRef, DragEvent, useState } from 'react';
+
+import {
+    LessonBlock,
+    LessonBlockProps,
+} from '@/features/dashboard/components/LessonBlock';
+import { LessonBlockFolder } from '@/features/dashboard/components/LessonBlockFolder';
+import { StyledCell } from '@/features/dashboard/styles/Timetable.style';
+
+export interface TimetableCellProps extends ComponentPropsWithRef<'div'> {
+    id: string;
+    lessonBlockProps: LessonBlockProps[];
+}
+
+/**
+ * Timetable cell component
+ */
+export const TimetableCell = ({
+    lessonBlockProps,
+    ...props
+}: TimetableCellProps) => {
+    const [isDraggedOver, setIsDraggedOver] = useState(false);
+    const handleDragover = (e: DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDraggedOver(true);
+    };
+    const handleDragleave = () => {
+        setIsDraggedOver(false);
+    };
+
+    const handleDrop = (e: DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsDraggedOver(false);
+        const data = e.dataTransfer.getData('text/plain');
+        //TODO: logic
+        console.log('Block:', data, '\nCell:', props.id);
+    };
+
+    return (
+        <StyledCell
+            {...props}
+            onDragOver={handleDragover}
+            onDrop={handleDrop}
+            onDragLeave={handleDragleave}
+            $isDraggedOver={isDraggedOver}
+        >
+            {lessonBlockProps.length === 1 && (
+                <LessonBlock {...lessonBlockProps[0]} />
+            )}
+            {lessonBlockProps.length > 1 && (
+                <LessonBlockFolder lessonBlockProps={lessonBlockProps} />
+            )}
+        </StyledCell>
+    );
+};
