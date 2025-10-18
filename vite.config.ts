0b40f3b470284df697b5dbefa8a49ffacbdcc493
ThePanToster <alcrafter@o2.pl>
2025-10-18 19:56:31 +0200

    Optimize the build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,20 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
+const manualChunks = [
+    'react-dom',
+    'react-router',
+    'react-select',
+    'react-mosaic-component',
+    'i18next',
+    'lodash',
+    '@tanstack',
+    'axios',
+    'react-toastify',
+    '@emotion',
+    'styled-components',
+];
+
 // https://vitejs.dev/config/
 export default defineConfig({
     test: {
@@ -23,6 +37,23 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src'),
+        },
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if (id.includes('node_modules')) {
+                        for (const lib of manualChunks) {
+                            if (id.includes(lib)) {
+                                return `vendor_${lib}`;
+                            }
+                        }
+
+                        return 'vendor';
+                    }
+                },
+            },
         },
     },
 });
