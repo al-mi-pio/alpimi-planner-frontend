96c2b18a34e61eac58edb1e07bede80552ab157e
ThePanToster <alcrafter@o2.pl>
2025-10-28 22:47:06 +0100

    Add post and delete for lessonPeriod

--- src/api/types/LessonPeriodService.ts
+++ src/api/types/LessonPeriodService.ts
@@ -1,7 +1,20 @@
-import type { ApiGetAllService, Entity } from '@/api/types';
+import type {
+    ApiDeleteService,
+    ApiGetAllService,
+    ApiPostService,
+    Entity,
+    Id,
+} from '@/api/types';
 
 export type LessonPeriod = Entity<{
     start: string;
 }>;
 
+export interface CreateLessonPeriodDTO {
+    scheduleId: Id;
+    start: string;
+}
+
 export type LessonPeriodGetAll = ApiGetAllService<LessonPeriod>;
+export type LessonPeriodPost = ApiPostService<CreateLessonPeriodDTO>;
+export type LessonPeriodDelete = ApiDeleteService;
