0402b28dd0b89e32cbf297fca585635f00d09a54
ThePanToster <alcrafter@o2.pl>
2025-10-29 23:33:43 +0100

    Add EditPage stories

---
+++ src/features/dashboard/pages/edit/index.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import EditPage from '@/features/dashboard/pages/edit/index';
+import Dashboard from '@/features/dashboard/template';
+import { editorDashboardHeader } from '@/features/schedules/constants';
+import { PageStoryStyles } from '@/shared/styles/Stories';
+
+export default {
+    title: 'Pages/Schedules Edit',
+    component: EditPage,
+    render: () => (
+        <Dashboard headerProps={editorDashboardHeader('mockScheduleName')}>
+            <PageStoryStyles />
+            <EditPage />
+        </Dashboard>
+    ),
+} satisfies Meta<typeof EditPage>;
+
+export const Default: StoryObj<typeof EditPage> = {};
