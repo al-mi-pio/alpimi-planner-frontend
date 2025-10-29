320f61e65f17c04061079868beeddf7ef6d157da
ThePanToster <alcrafter@o2.pl>
2025-10-29 01:51:23 +0100

    Fix router and mock data in storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,13 +2,14 @@ import type { Preview } from '@storybook/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Suspense, useEffect } from 'react';
 import { I18nextProvider } from 'react-i18next';
-import { MemoryRouter } from 'react-router';
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 
 import { http, HttpResponse } from 'msw';
 import { initialize, mswLoader } from 'msw-storybook-addon';
 import { ThemeProvider } from 'styled-components';
 
 import { authRefreshUrl } from '@/api/services/authService';
+import Page404 from '@/features/main/pages/404';
 import i18n from '@/i18n';
 import { GlobalStyle } from '@/main.style';
 import { GlobalToastStyles } from '@/shared/components/Toast/Toast.style';
@@ -23,7 +24,13 @@ initialize({
     onUnhandledRequest: 'bypass',
 });
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: Infinity,
+        },
+    },
+});
 
 export const globalTypes = {
     theme: {
@@ -82,32 +89,131 @@ const preview: Preview = {
         },
     },
     tags: ['autodocs'],
-    loaders: [mswLoader],
+    loaders: [
+        mswLoader,
+        () => {
+            window.localStorage.setItem(
+                'accessToken',
+                'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNb2NrIElzc3VlciIsImlhdCI6MTc2MTY5MDI5OSwiZXhwIjoxNzkzMjI2Mjk5LCJhdWQiOiJ3d3cuZXhhbXBsZS5jb20iLCJzdWIiOiJtb2NrQGV4YW1wbGUuY29tIiwibG9naW4iOiJtb2NrVXNlciIsInVzZXJJZCI6IjAtMC0wLTAtMSJ9.SAHfsFlQoe-K4u7FIydljSmLHNuAS-jpJoe6f9ayPKQ'
+            );
+        },
+    ],
     decorators: [
         (Story, context) => {
             const theme = context.parameters.theme || context.globals.theme;
             const { locale } = context.globals;
 
+            queryClient.setQueryData(['user', '0-0-0-0-1'], {
+                content: {
+                    id: '0-0-0-0-1',
+                    login: 'mockUser',
+                    customURL: 'mockUrl',
+                },
+            });
+            queryClient.setQueryData(
+                ['schedule', 'mockUrl', 'mockScheduleName'],
+                {
+                    content: {
+                        id: '0-0-0-0-2',
+                        name: 'mockScheduleName',
+                    },
+                }
+            );
+            queryClient.setQueryData(['lessonPeriod'], {
+                content: [
+                    {
+                        id: '0-0-1-0-0',
+                        start: '08:00:00',
+                    },
+                ],
+            });
+            queryClient.setQueryData(['lesson'], {
+                content: [],
+            });
+            queryClient.setQueryData(['group'], {
+                content: [],
+            });
+            queryClient.setQueryData(['subgroup'], {
+                content: [],
+            });
+            queryClient.setQueryData(['collision'], {
+                content: [],
+            });
+            queryClient.setQueryData(
+                ['lessonBlock', '0-0-0-0-2', '2025-10-27'],
+                {
+                    content: [],
+                }
+            );
+            queryClient.setQueryData(['lessonBlock', '0-0-0-0-2'], {
+                content: [],
+            });
+            queryClient.setQueryData(['teacher', '0-0-0-0-2'], {
+                content: [],
+            });
+            queryClient.setQueryData(['dayOff'], {
+                content: [],
+            });
+            queryClient.setQueryData(['classroom', '0-0-0-0-2'], {
+                content: [],
+            });
+            queryClient.setQueryData(['scheduleSettings'], {
+                content: {
+                    id: '0-0-0-0-3',
+                    schoolHour: 45,
+                    schoolYearStart: '2025-10-30',
+                    schoolYearEnd: '2026-10-30',
+                    schoolDays: '0111110',
+                    isPublic: false,
+                },
+            });
+
+            const router = createMemoryRouter(
+                [
+                    {
+                        path: '/:scheduleName',
+                        loader: ({ params }) => ({
+                            scheduleName: params.scheduleName,
+                        }),
+                        element: (
+                            <Suspense
+                                fallback={<div>loading translations...</div>}
+                            >
+                                <I18nextProvider i18n={i18n}>
+                                    <ThemeProvider
+                                        theme={
+                                            theme === 'light'
+                                                ? lightTheme
+                                                : darkTheme
+                                        }
+                                    >
+                                        <QueryClientProvider
+                                            client={queryClient}
+                                        >
+                                            <GlobalStyle />
+                                            <GlobalToastStyles />
+
+                                            <Story />
+                                        </QueryClientProvider>
+                                    </ThemeProvider>
+                                </I18nextProvider>
+                            </Suspense>
+                        ),
+                    },
+                    {
+                        path: '*',
+                        element: <Page404 />,
+                    },
+                ],
+                {
+                    initialEntries: ['/mockScheduleName'],
+                }
+            );
+
             useEffect(() => {
                 i18n.changeLanguage(locale).then();
             }, [locale]);
-            return (
-                <Suspense fallback={<div>loading translations...</div>}>
-                    <I18nextProvider i18n={i18n}>
-                        <ThemeProvider
-                            theme={theme === 'light' ? lightTheme : darkTheme}
-                        >
-                            <QueryClientProvider client={queryClient}>
-                                <GlobalStyle />
-                                <GlobalToastStyles />
-                                <MemoryRouter>
-                                    <Story />
-                                </MemoryRouter>
-                            </QueryClientProvider>
-                        </ThemeProvider>
-                    </I18nextProvider>
-                </Suspense>
-            );
+            return <RouterProvider router={router} />;
         },
     ],
 };
