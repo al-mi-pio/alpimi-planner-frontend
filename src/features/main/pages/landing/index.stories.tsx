582107bfb52d50ee55d7aa8f17e3661ad309c348
ThePanToster <alcrafter@o2.pl>
2025-10-31 17:22:47 +0100

    Add stories for the Landing page

---
+++ src/features/main/pages/landing/index.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import LandingPage from '@/features/main/pages/landing/index';
+import { PageStoryStyles } from '@/shared/styles/Stories';
+
+export default {
+    title: 'Pages/Landing',
+    tags: ['!autodocs'],
+    component: LandingPage,
+    render: () => (
+        <>
+            <PageStoryStyles />
+            <LandingPage />
+        </>
+    ),
+} satisfies Meta<typeof LandingPage>;
+
+export const Default: StoryObj<typeof LandingPage> = {};
