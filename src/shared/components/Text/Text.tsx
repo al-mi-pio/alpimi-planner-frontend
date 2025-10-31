6787f482b4ac34a215427a7d3ae16ec5b155745f
ThePanToster <alcrafter@o2.pl>
2025-10-31 01:13:48 +0100

    Add textarea support for Text component

--- src/shared/components/Text/Text.tsx
+++ src/shared/components/Text/Text.tsx
@@ -1,7 +1,7 @@
 import { type ComponentPropsWithRef, useId } from 'react';
 
 import type { TextTypes } from '@/shared/components/Text/types';
-import { StyledInput } from '@/shared/styles/Common';
+import { StyledInput, StyledTextarea } from '@/shared/styles/Common';
 import { ErrorDescription, Label } from '@/shared/styles/Strings';
 
 export interface TextProps
@@ -18,23 +18,46 @@ export interface TextProps
      * Optional error message underneath the field, also highlights the input
      */
     error?: string;
+    /**
+     * Optional flag that renders a textarea instead of text input
+     */
+    multiline?: boolean;
+    rows?: number;
 }
 
 /**
  * A UI component which accepts user text input
  */
-const Text = ({ type = 'text', label, error, ...defaultProps }: TextProps) => {
+const Text = ({
+    type = 'text',
+    label,
+    error,
+    multiline,
+    rows,
+    ...defaultProps
+}: TextProps) => {
     const errorDescriptionId = useId();
     return (
         <label>
             {label && <Label>{label}</Label>}
-            <StyledInput
-                $error={!!error}
-                aria-invalid={!!error}
-                aria-describedby={error ? errorDescriptionId : undefined}
-                type={type}
-                {...defaultProps}
-            />
+            {multiline ? (
+                <StyledTextarea
+                    $error={!!error}
+                    aria-invalid={!!error}
+                    aria-describedby={error ? errorDescriptionId : undefined}
+                    rows={rows}
+                    {...(defaultProps as ComponentPropsWithRef<'textarea'>)}
+                />
+            ) : (
+                <StyledInput
+                    $error={!!error}
+                    aria-invalid={!!error}
+                    aria-describedby={error ? errorDescriptionId : undefined}
+                    type={type}
+                    {...defaultProps}
+                />
+            )}
+
             {error && (
                 <ErrorDescription role="alert" id={errorDescriptionId}>
                     {error}
